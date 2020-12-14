953740ba123ab77e124c9a15fe48d05527393f16
Jay Rawlings <jrawlings@sensoryinc.com>
2020-12-14 08:14:50 -0700

    Add initial day4 work

--- 4/day4.py
+++ 4/day4.py
@@ -3,21 +3,40 @@ import sys
 
 def main():
     input_lines = get_input()
-    print(get_valid_count(input_lines))
+    get_valid_count(input_lines)
+    # print(get_valid_count(input_lines))
 
 
-def get_valid_count():
+def get_valid_count(lines):
     valid_count = 0
+    passport = []
+    passports = []
+    skip = before = after = 0
 
+    for line in lines:
+    	print(line)
+    	if len(line) == 0:
+    		passports.append(passport)
+    		passports = []
+    	else:
+    		print('else')
+    		passport.append(line)
+		print(passport)
+
+	print(len(lines))
+    # print(skip)
+    # print(before)
+    # print(after)
+    print(passports)
     
 
     return valid_count
 
 
-def get_input(input_file=input.txt):
+def get_input(input_file='day4_input.txt'):
     with open (input_file, 'r') as f:
         return [line.rstrip('\n') for line in f]
-
+        # f.read()
 
 if __name__ == '__main__':
     main()
