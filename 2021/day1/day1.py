2663264ea2525d6bfd453d9d79d8a53b8b0536f8
Jay Rawlings <jay_rawlings7@hotmail.com>
2021-12-01 15:42:09 -0700

    Fix and simplify day1 code

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -2,18 +2,15 @@ import sys
 
 increases = 0
 avg_increases = 0
-i = 1
 
 with open(sys.argv[1], 'r') as f:
     depths = [int(line) for line in f]
 
-while i < len(depths):
-    if depths[i] > depths[i-1]:
+for i, depth in enumerate(depths):
+    if i > 0 and depths[i] > depths[i-1]:
         increases += 1
     if i > 2 and sum(depths[i-3:i]) < sum(depths[i-2:i+1]):
         avg_increases += 1
-    i += 1
 
 print(f'Part 1 answer: {increases}')
 print(f'Part 2 answer: {avg_increases}')
-
