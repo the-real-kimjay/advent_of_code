c7298a09492f35cdc673d0bb4fe0e1c0dcbaec0e
Jay Rawlings <jay_rawlings7@hotmail.com>
2021-12-02 11:36:05 -0700

    Missed obvious simplification on day1 solution

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,16 +1,19 @@
 import sys
 
-increases = 0
-avg_increases = 0
 
-with open(sys.argv[1], 'r') as f:
-    depths = [int(line) for line in f]
+def get_input():
+    with open(sys.argv[1], 'r') as f:
+        return [int(line) for line in f]
 
-for i in range(len(depths)):
-    if i > 0 and depths[i] > depths[i-1]:
-        increases += 1
-    if i > 2 and sum(depths[i-3:i]) < sum(depths[i-2:i+1]):
-        avg_increases += 1
 
-print(f'Part 1 answer: {increases}')
-print(f'Part 2 answer: {avg_increases}')
+def get_increases(n):
+    depths = get_input()
+    increases = 0
+    for i in range(n, len(depths)):
+        if depths[i] > depths[i-n]:
+            increases += 1
+    return increases
+
+
+print(f'Part 1 answer: {get_increases(1)}')
+print(f'Part 2 answer: {get_increases(3)}')
